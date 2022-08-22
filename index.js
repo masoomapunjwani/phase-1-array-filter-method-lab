d568d2f68fd74a289eded5887eb4bce814ce450d
masoomahasnain <masooma.punjwani@student.moringaschool.com>
2022-08-22 22:40:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(driversArray, nameString){
+    return driversArray.filter( (name) => {
+        return name.toUpperCase() === nameString.toUpperCase();
+    });
+};
+
+function fuzzyMatch(driversArray, nameString){
+    return driversArray.filter( (name) => {
+        return name[0].toUpperCase() === nameString[0].toUpperCase();
+    
+    });
+};
+
+function matchName(driversArray, nameString){
+    return driversArray.filter( (driverName) => {
+        return driverName.name === nameString;
+    });
+};
